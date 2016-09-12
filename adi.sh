a49a85bb71b45cb04ecceb4e11aade2b27a9e3d6
Jean-Simon Desjardins <jsdesjardinssds@gmail.com>
2016-09-12 17:54:00 -0400

    Create adi.sh
    This is an automatic deb installer written in bash (for Linux). It replaces the bug in Ubuntu 16.04 LTS version with which it isn't possible to install any third-party software (installation crashes). You can install those deb files with commands or use my program to do it automatically !

---
+++ adi.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# Potential bugs:
+# none for now
+
+# Declaring colors
+YEL='\033[1;33m'
+WHT='\033[1;37m'
+RED='\033[1;31m' # This is actually Light red
+LGRN='\033[1;32m'
+LCYN='\033[1;36m'
+NC='\033[0m'
+
+#changing directory
+cd "$HOME/Downloads"
+
+
+# Ask for name of file
+function filename {
+ echo -e "${LGRN}Adi.sh (Automatic Deb Installer) by Jean-Simon Desjardins - https://github.com/jsgarden"
+ echo
+ echo -e "${YEL}Name of file ? (.deb)${WHT}"
+ read flnm
+ 
+ echo
+
+ echo -e "${YEL}Are you sure ? (Y/N)${WHT}"
+ read yn_flnm
+
+ if [ "$yn_flnm" == "y" ]
+	then
+		cmpl
+		
+ elif [ "$yn_flnm" == "n" ]
+	then
+		filename
+		echo
+		
+ fi }
+
+
+
+# Function for the sudo dpkg command
+function cmpl {
+ echo
+ echo -e "${YEL}Launching Compilation${NC}"
+ sleep 1
+ sudo dpkg -i $flnm
+ 
+ instl
+ }
+ 
+ 
+ 
+# Function for forced apt-get install
+function instl {
+
+ echo
+ echo -e "${YEL}Launching Installation${NC}"
+ sleep 1
+ 
+ sudo apt-get install -f
+ echo
+ 
+ echo -e "${LGRN}Finished !"
+ sleep 1
+ echo
+ autoremove
+ echo
+}
+
+function autoremove {
+ echo -e "${LCYN}Do you wish to run autoremove ? (Y/N) ${WHT}"
+ read autorm
+ 
+ if [ "$autorm" == "y" ]
+	then
+		echo
+		echo -e "${LCYN}Cleaning...${NC}"
+		sleep 1
+		sudo apt-get autoremove
+		echo
+		echo -e "${LCYN}Done !${NC}"
+		echo
+		echo
+		sleep 1
+		restart
+		
+ elif ["$autorm" == "n" ]
+	then
+		restart
+fi }
+ 
+ function restart {
+ echo -e "${YEL}Restart (R) or exit (E) ?${WHT}"
+ read re_instl
+ echo
+ 
+ if [ "$re_instl" == "r" ]
+	then
+		filename
+		echo
+
+elif [ "$re_instl" == "e" ]
+	then
+		echo -e "${RED}Exiting..."
+		sleep 1
+		exit
+fi }
+
+
+# Start the program
+filename
